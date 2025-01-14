fd0179e6e9025be80279fb650d0966352dd60441
Suresh Thapa <zorosenpai456@gmail.com>
2025-01-14 09:24:41 +0545

    commit

--- components/Home.tsx
+++ components/Home.tsx
@@ -32,10 +32,12 @@ export default function Home() {
 
           {/* Button */}
           <div className="flex items-center gap-6">
-            <Button>
-              <CloudDownload className="w-5 h-5" />
-              <span>Download Resume</span>
-            </Button>
+            <a href="/CV.pdf" download="bishal_dahal_khatri_cv.pdf">
+              <Button>
+                <CloudDownload className="w-5 h-5" />
+                <span>Download Resume</span>
+              </Button>
+            </a>
             <Button>
               <Mail />
               <span>Contact Me</span>
